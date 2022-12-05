9664324a7631e94391a10763f16540d831e0ddce
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-12-05 13:51:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.left = 'left'
+//  var left = dodger.style.position = "left";
+//  left = parseInt(left);
+
+
+// 
+function moveDodgerLeft() {
+	let leftValue = dodger.style.left.replace("px", "");
+	let leftInt = parseInt(leftValue, );
+	if (leftInt > 0) {
+		dodger.style.left = `${leftInt - 1}px`;
+	}
+}
+function moveDodgerRight() {
+	var rightvalue = dodger.style.left.replace("px", "");
+	var rightInt = parseInt(rightvalue, );
+	if (rightInt < 180) {
+		dodger.style.left = `${rightInt + 1}px`;
+	}
+}
+document.addEventListener("keydown", function (d) {
+	if (d.have === 37) {
+		moveDodgerLeft();
+	}
+	if (d.have === 39) {
+		moveDodgerRight();
+	}
+});
